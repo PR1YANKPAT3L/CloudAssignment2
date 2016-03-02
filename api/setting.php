ffff7995129fdb34cf42678e30ba1966ed585920
PR1YANKPAT3L <silentcoder>
2016-03-02 14:38:25 -0500

    Fixed RDS config

--- api/setting.php
+++ api/setting.php
@@ -2,7 +2,7 @@
 
 define('HASH', 'cloudapp2');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', 'devsql');
-define('DB_NAME', 'clapp');
+define('DB_HOST', 'cloudprojectdb.cg4fbiqotneu.us-west-2.rds.amazonaws.com');
+define('DB_USER', 'silentcoder');
+define('DB_PASS', 'mad123bhere');
+define('DB_NAME', 'cloudproject2');
